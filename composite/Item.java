3e5e8f02ec1128ae7dda30460ed8b956a6bcf73e
stanrazvan <75726079+stanrazvan@users.noreply.github.com>
2022-05-31 19:48:57 +0300

    Add files via upload

---
+++ composite/Item.java
@@ -0,0 +1,10 @@
+package composite;
+
+public interface Item {
+    public void adaugaItem(Item item);
+    public void stergeItem(Item item);
+    public void descrieItem();
+    public Item getItem(int position);
+    public float calculeazSumaGarantata(float pretPerLoc);
+
+}
