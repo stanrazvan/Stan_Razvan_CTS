e1a2d2662a3e666b4f93e993a3e4d8762d96e53c
stanrazvan <75726079+stanrazvan@users.noreply.github.com>
2022-05-31 19:52:41 +0300

    Add files via upload

---
+++ flyweight/Flyweight.java
@@ -0,0 +1,7 @@
+package flyweight;
+
+public interface Flyweight {
+
+	public void afiseazaDescriereAutobuz(Autobuz autobuz);
+	
+}
---
+++ flyweight/LinieFactory.java
@@ -0,0 +1,25 @@
+package flyweight;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LinieFactory {
+	
+	Map<Integer,Linie> mapFactory;
+
+	public LinieFactory() {
+		this.mapFactory=new HashMap<>();
+	}
+	
+	public Linie getLinie(int nrLinie) {
+		if(mapFactory.containsKey(nrLinie)) {
+			return mapFactory.get(nrLinie);
+		}else {
+			Linie linie=new Linie(nrLinie,"prima statie","ultima statie");
+			mapFactory.put(nrLinie, linie);
+			return linie;
+		}
+	}
+	
+
+}
